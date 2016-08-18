d2100400f7f2356527f4e50605dec5489d32b99f
reddberckley <danielonodje@gmail.com>
2016-08-18 14:34:48 +0100

    added get and list features

--- index.js
+++ index.js
@@ -3,12 +3,26 @@
 var notesapp = function(author){
     this.author = author
     this.notes = [];
-}
 
-// notesapp.prototype.notes = [];
-
-notesapp.prototype.addNote = function(note_content) {
+    this.addNote = function(note_content) {
         this.notes.push(note_content);
     }
+
+    this.editNote = function(note_id,note_content) {
+        this.notes[note_id] = note_content;
+    }
+
+    this.listNotes = function() {
+        let rnotes = [];    
+        for(let i=0;i<this.notes.length;i++) {
+            rnotes.push(this.notes[i]);
+        }
+        return rnotes;
+    }
+
+    this.getNote = function(note_id) {
+        return this.notes[note_id];
+    }
+}
 
 module.exports = notesapp;
--- test/test.js
+++ test/test.js
@@ -1,21 +1,53 @@
 var notesapp =require('../');
-var notes = new notesapp();
 var chai = require('chai');
 var assert = chai.assert;
 
 describe("NotesApp", function() {
     describe("constructor",function(){
         it("should create a new notes app instance", function() {
+            var notes = new notesapp();
             assert.typeOf(notes,"object");
         });
     }); 
 
     describe("addNote",function(){
         it("should add a new note",function(){
+            var notes = new notesapp();
             var intialsize = notes.notes.length;
             notes.addNote("a note");
             var newsize = notes.notes.length;
             assert.operator(newsize, '>', intialsize);
+        })
+    });
+
+    describe("editNote",function(){
+        it("should edit an existing note",function(){
+            notes = new notesapp();
+            notes.addNote("a note");
+            notes.editNote(0,"check note");
+            assert.equal(notes.notes[0],"check note");
+        })
+    });
+
+    describe("listNotes",function(){
+        it("should list all existing notes",function(){
+            notes = new notesapp();
+            notes.addNote("a note");
+            notes.addNote("another note");
+            notesarray = notes.listNotes();
+            assert.lengthOf(notesarray,2);
+            assert.equal(notesarray[0],"a note");
+            assert.equal(notesarray[1],"another note");            
+        })
+    });
+
+    describe("getNote",function(){
+        it("should return the requested note",function(){
+            notes = new notesapp();
+            notes.addNote("a note");
+            var note = notes.getNote(0);
+            assert.typeOf(note,'string');
+            assert.equal(note,'a note');
         })
     })
 })
